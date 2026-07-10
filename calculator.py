9d07508d8a334dbcee3da95899abbb338fcb4579
Samuel Mensah <rextony2gal@gmail.com>
2026-07-10 15:35:27 -0400

    Introduce bug for CI demo - Samuel Mensah

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 # calculator.py - Created by Firstname Lastname
 
 def add(a, b):
-    return a - b # Bug: subtracting instead of adding
+    return a - b   # Bug: subtracting instead of adding
 
 
 def subtract(a, b):
